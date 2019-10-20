d6a72e6e4fb7d7de6c4736d084755b771fb160d5
Yuanyi Wu <wuyuanyi135@gmail.com>
2019-10-20 16:12:37 -0400

    camera periph working

--- services/hardware_control.py
+++ services/hardware_control.py
@@ -60,19 +60,21 @@ class SerialCameraPeripheralControl(CameraPeripheralControl):
         ])
 
     def start(self):
-        self.serial = serial.Serial(self.config["serial_port"], baudrate=self.config["baud"])
         try:
-            self.serial.open()
+            self.serial = serial.Serial(self.config["serial_port"], baudrate=self.config["baud"])
         except Exception as ex:
             self.logger.error(f"Failed to open {self.config['serial_port']}.")
             self.logger.debug(ex)
             return
 
+        self.logger.info("Successfully connect to camera peripheral control.")
         super().start()
 
     def stop(self):
         if self.serial:
             self.serial.close()
+
+        self.logger.info("Successfully disconnect from camera peripheral control")
         super().stop()
 
     def set_power(self, enabled: bool):
@@ -82,7 +84,7 @@ class SerialCameraPeripheralControl(CameraPeripheralControl):
             val = 1 if enabled else 0
             if self.stored_parameters.power != val and val is not None:
                 self.stored_parameters.power = val
-                self.serial.write(f"s_power {val}\n")
+                self.serial.write(f"s_power {val}\n".encode())
         except Exception as ex:
             self.logger.error(f"Failed to set power: {ex}")
             self.logger.debug(traceback.format_exc())
@@ -102,15 +104,15 @@ class SerialCameraPeripheralControl(CameraPeripheralControl):
 
             if param.digital_filter != self.stored_parameters.digital_filter and param.digital_filter is not None:
                 self.stored_parameters.digital_filter = param.digital_filter
-                self.serial.write(f"s_filter {param.digital_filter}\ncommit\n")
+                self.serial.write(f"s_filter {param.digital_filter}\ncommit\n".encode())
                 self.logger.debug(f"digital filter updated to {param.digital_filter}")
             if param.pulse_width_tick != self.stored_parameters.pulse_width_tick and param.pulse_width_tick is not None:
                 self.stored_parameters.pulse_width_tick = param.pulse_width_tick
-                self.serial.write(f"s_exposure {param.pulse_width_tick}\ncommit\n")
+                self.serial.write(f"s_exposure {param.pulse_width_tick}\ncommit\n".encode())
                 self.logger.debug(f"Pulse width updated to {param.pulse_width_tick}")
             if param.start_delay != self.stored_parameters.start_delay and param.start_delay is not None:
                 self.stored_parameters.start_delay = param.start_delay
-                self.serial.write(f"s_delay {param.start_delay}\ncommit\n")
+                self.serial.write(f"s_delay {param.start_delay}\ncommit\n".encode())
                 self.logger.debug(f"Start delay updated to {param.start_delay}")
         except Exception as ex:
             self.logger.error(f"Failed to set parameters: {ex}")
@@ -121,9 +123,9 @@ class SerialCameraPeripheralControl(CameraPeripheralControl):
             self.guard_serial_open()
             if self.stored_parameters.trigger != enabled and enabled is not None:
                 if enabled:
-                    self.serial.write("arm_trigger\n")
+                    self.serial.write("arm_trigger\n".encode())
                 else:
-                    self.serial.write("disarm_trigger\n")
+                    self.serial.write("disarm_trigger\n".encode())
                 self.stored_parameters.trigger = enabled
                 super().set_trigger(enabled)
         except Exception as ex:
